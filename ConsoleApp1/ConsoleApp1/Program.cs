633bb7caa72eb27ca1a4c3eacc58850eef614abc
John Doe <john.doe@gmail.com>
2019-07-10 10:22:07 -0300

    é isto

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git");
+            string[] nameList = new string[2] {"Valor 1", "Valor 2" };
+
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firstTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("yyyy/MM/dd/hh:mm:ss.fff");
+                Console.WriteLine(dateList[i]);
+            }
+            //Thread.Sleep(100);
+
+
+            Console.WriteLine((DateTime.Now - firstTime).ToString());
+
+            Console.ReadKey();
 
         }
     }
