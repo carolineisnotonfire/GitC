7964ee7cc14ccbde99ca1c8478924b69990c4e1b
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-16 09:26:50 -0300

    ABANDONEI A GABRIELA

--- ListaExemplo/Program.cs
+++ ListaExemplo/Program.cs
@@ -10,101 +10,132 @@ namespace ListaExemplo
     {
         static void Main(string[] args)
         {
-            //lista com tres valores um id e outras duas que o usuario coloque
-
-            string[,] listaMusica = new string[5, 3];
-
+            string[,] listaMusica = new string[2, 5];
             int IDLista = 0;
+            Console.WriteLine("Iniciando sistema de lista de nome e idade.");
+            var escolhaInicial = MenuInicial();
+            while (true)
+            {
+                switch (escolhaInicial)
+                {
+                    case "1": { InsereRegistro(ref listaMusica, ref IDLista); } break;
+                    case "2": { Deleta(ref listaMusica); } break;
+                    case "3": { MostrarInformacoes(listaMusica); } break;
+                    case "4": { MostrarInformacoes(listaMusica, "true"); } break;
+                    case "5": { return; }
+                }
+                //Alimento a escolha novamente 
+                escolhaInicial = MenuInicial();
+            }
 
-            InsereRegistro(ref listaMusica, ref IDLista);
-            Deleta(ref listaMusica);
-
-            Console.ReadKey();
         }
-        public static void InsereRegistro (ref string [,] listaMusica, ref int IDLista)
+        public static string MenuInicial()
         {
+            Console.Clear();
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 - Inseir um novo registro.");
+            Console.WriteLine("2 - Remover um novo registro.");
+            Console.WriteLine("3 - Listar informações.");
+            Console.WriteLine("4 - Listar informações desativadas.");
+            Console.WriteLine("5 - Sair do sistema.");
+
+            Console.WriteLine("Digite o número da opção desejada:");
+            //retorna diretamente o menu escolhido.
+            return Console.ReadLine();
+        }
+        public static void InsereRegistro(ref string[,] listaMusica, ref int IDLista)
+        {
+            Console.WriteLine("Álbum: ");
+            var album = Console.ReadLine();
+            Console.WriteLine("Ano: ");
+            var ano = Console.ReadLine();
+            AumentaTamanho(ref listaMusica);
             for (int i = 0; i < listaMusica.GetLength(0); i++)
             {
                 if (listaMusica[i, 0] != null)
                     continue;
 
-                Console.WriteLine("Álbum: ");
-                var album = Console.ReadLine();
-
                 listaMusica[i, 0] = (IDLista++).ToString();
                 listaMusica[i, 1] = album;
-
-                Console.WriteLine("Ano: ");
-                var ano = Console.ReadLine();
-
                 listaMusica[i, 2] = ano;
-
-                Console.WriteLine("Inserir novo registro? sim (y) não (n)");
-                var continuar = Console.ReadKey().KeyChar.ToString();
-                Console.Clear();
-                if (continuar == "n")
-
-                    break;
-                AumentaTamanho(ref listaMusica);
-
+                listaMusica[i, 3] = "true";
+                listaMusica[i, 4] = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+                break;
             }
-
-                Console.WriteLine("Lista de álbuns: ");
-
-                for (int i = 0; i < listaMusica.GetLength(0); i++)
-
-                    Console.WriteLine(string.Format("ID do Álbum: {0 } Álbum: {1} Ano: {2}", listaMusica[i, 0], listaMusica[i, 1], listaMusica[i, 2]));
-                Console.ReadKey();
-
-        }
-        public static void AumentaTamanho(ref string[,] listaMusica)
-        {
-            var limiteLista = true;
-            for (int i = 0; i < listaMusica.GetLength(0); i++)
-            {
-                if (listaMusica [i,0] == null)
-                
-                    limiteLista = false;
-                
-
-            }
-
-            if (limiteLista)
-            {
-                var listaCopia = listaMusica;
-
-                listaMusica = new string[listaMusica.GetLength(0) + 5, 3];
-
-                for (int i = 0; i < listaCopia.GetLength(0); i++)
-                {
-                    listaMusica[i, 0] = listaCopia[i, 0];
-                    listaMusica[i, 1] = listaCopia[i, 1];
-                    listaMusica[i, 2] = listaCopia[i, 2];
-                }
-
-                Console.WriteLine("Tamanho da lista atualizado!");
-            }
+            Console.WriteLine("Registro cadastrado!");
+            Console.WriteLine("Aperte qualquer tecla para voltar ao menu inicial");
+            Console.ReadKey();
         }
         public static void Deleta(ref string[,] listaMusica)
         {
 
             for (int i = 0; i < listaMusica.GetLength(0); i++)
-                Console.WriteLine($"ID: {listaMusica[i,0]} Nome: {listaMusica[i,1]} Idade {listaMusica[i,2]}");
+                Console.WriteLine($"ID: {listaMusica[i, 0]} Álbum: {listaMusica[i, 1]} Ano: {listaMusica[i, 2]}");
 
             Console.WriteLine("ID de registro a ser removido: ");
             var id = Console.ReadLine();
 
             for (int i = 0; i < listaMusica.GetLength(0); i++)
             {
-                if (listaMusica[i,0] == id)
+                if (listaMusica[i, 0] == id)
                 {
-                    listaMusica[i, 0] = null;
-                    listaMusica[i, 1] = null;
-                    listaMusica[i, 2] = null;
+                    listaMusica[i, 3] = "false";
+                    listaMusica[i, 4] = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
                 }
             }
 
+            Console.WriteLine("Para retornar ao menu inicial, aperte qualquer tecla");
+            Console.ReadKey();
+
 
         }
+        public static void MostrarInformacoes(string[,] listaMusica, string mostrarRegistrosNAtivos = "false")
+        {
+            Console.WriteLine("Lista de álbuns: ");
+            if (mostrarRegistrosNAtivos == "true")
+            
+                Console.WriteLine("Registros desativados dentro do sistema.");
+            
+
+            for (int i = 0; i < listaMusica.GetLength(0); i++)
+            {
+                if (listaMusica[i, 3] != mostrarRegistrosNAtivos)
+                Console.WriteLine($"ID {listaMusica[i, 0]} " +
+                          $"- Álbum:{listaMusica[i, 1]} " +
+                          $"- Ano:{listaMusica[i, 2]}" +
+                          $"- Data Alteração:{listaMusica[i, 4]}");
+            }
+            Console.WriteLine("Resultados apresentados com sucesso!");
+            Console.WriteLine("Para voltar ao menu inicial informar qualquer tecla.");
+            Console.ReadKey();
+        }
+        public static void AumentaTamanho(ref string[,] listaMusica)
+        {
+            var limiteLista = true;
+            for (int i = 0; i < listaMusica.GetLength(0); i++)
+            {
+                if (listaMusica[i, 0] == null)
+                    limiteLista = false;
+            }
+
+            if (limiteLista)
+            {
+                var listaCopia = listaMusica;
+
+                listaMusica = new string[listaMusica.GetLength(0) + 5, 5];
+
+                for (int i = 0; i < listaCopia.GetLength(0); i++)
+                {
+                    listaMusica[i, 0] = listaCopia[i, 0];
+                    listaMusica[i, 1] = listaCopia[i, 1];
+                    listaMusica[i, 2] = listaCopia[i, 2];
+                    listaMusica[i, 3] = listaCopia[i, 3];
+                    listaMusica[i, 4] = listaCopia[i, 4];
+                }
+                Console.WriteLine("Tamanho da lista atualizado!");
+            }
+        }
+
     }
 }
+
