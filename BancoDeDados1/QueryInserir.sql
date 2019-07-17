c12448ba21334987a2a1ef99f7d46eeade4e0d72
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 11:14:33 -0300

    query nova rapeize

---
+++ BancoDeDados1/QueryInserir.sql
@@ -0,0 +1,17 @@
+﻿insert into AulaCsharp 
+(Aluno, Idade, ativo, UsuAlt, UsuInc, DatAlt, DatInc) values 
+('Allan	Patton', 18, 1, 26, 26, getdate(), getdate());
+insert into AulaCsharp 
+(Aluno, Idade, ativo, UsuAlt, UsuInc, DatAlt, DatInc) values 
+('Tonya	Logan', 17, 1, 26, 26, getdate(), getdate());
+insert into AulaCsharp 
+(Aluno, Idade, ativo, UsuAlt, UsuInc, DatAlt, DatInc) values 
+('Ramon	Adkins', 38, 0, 26, 26, getdate(), getdate());
+insert into AulaCsharp 
+(Aluno, Idade, ativo, UsuAlt, UsuInc, DatAlt, DatInc) values 
+('Marian Mathis', 25, 1, 26, 26, getdate(), getdate());
+insert into AulaCsharp 
+(Aluno, Idade, ativo, UsuAlt, UsuInc, DatAlt, DatInc) values 
+('Joann	Lambert', 43, 1, 26, 26, getdate(), getdate());
+
+select * from AulaCsharp;
