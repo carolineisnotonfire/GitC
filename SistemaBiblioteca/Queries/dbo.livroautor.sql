25716c6dd1ed48ef576013b725b5e76e99b31352
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 17:21:11 -0300

    committttttt

---
+++ SistemaBiblioteca/Queries/dbo.livroautor.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LivroAutor] (
+    [Livro] INT NOT NULL,
+    [Autor] INT NOT NULL,
+    CONSTRAINT [FK_Table_Livros] FOREIGN KEY ([Livro]) REFERENCES [dbo].[Livros] ([Id]),
+    CONSTRAINT [FK_Table_Autores] FOREIGN KEY ([Autor]) REFERENCES [dbo].[Autores] ([Id])
+);
+
