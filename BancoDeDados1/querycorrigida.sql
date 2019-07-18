4fb94294435906549aac9fb064226e4ecbaefe12
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 09:08:17 -0300

    sql commands

---
+++ BancoDeDados1/querycorrigida.sql
@@ -0,0 +1,15 @@
+﻿--select TipoDog, (sum(TotalVendas)) as 'Vendas',
+--MONTH(DataVendas) as 'Mes'
+--
+--from FTZEZE 
+--group by TipoDog, DataVendas
+--order by TipoDog
+--
+--select * from FTZEZE where TipoDog like 'Dog Soya'
+
+select TipoDog, sum(TotalVendas) as 'Vendas', MONTH(DataVendas) as 'Mês'
+from FTZEZE --where MONTH(DataVendas) between 1 and 2
+group by TipoDog, MONTH(DataVendas)
+order by 'Vendas' desc
+
+--correção do professor
