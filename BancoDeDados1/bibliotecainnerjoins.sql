9d25a7ee50775ee789876c93a558dd4a4972e837
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 14:19:30 -0300

    Commit

---
+++ BancoDeDados1/bibliotecainnerjoins.sql
@@ -0,0 +1,20 @@
+﻿--select * from Livros
+--select * from Biblioteca
+--select Livros.Nome , Biblioteca.Nome fro
+--
+--select 
+--	BL.Nome,
+--	count(LV.Id) as 'Quantidade de livros'
+--
+--from Biblioteca BL
+--inner join Livros LV on BL.Id = LV.Biblioteca
+--group by BL.Nome
+
+select BL.Nome,
+count (LV.Id) as 'Quantidade de livros',
+US.Nome as 'Username' from Biblioteca BL
+
+inner join Livros LV on BL.Id = LV.Biblioteca
+inner join Usuario US on LV.UsuInc = US.Id
+
+group by BL.Nome, US.Nome
