e46acfc6809ae5607bfc3d8acf85621249e438f9
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 16:20:50 -0300

    banco de dados exercício boletim

---
+++ Exercícios Banco de Dados/ExercicioBoletim.sql
@@ -0,0 +1,10 @@
+﻿select 
+A.Nome,
+sum(NT.Nota) / count(NT.Nota) as 'media',
+iif ((sum(NT.Nota) / count(NT.Nota)) >= 7, 'Aprovado', 'Reprovado') as 'status media',
+iif (Sum(convert(money, NT.Presenca)) / convert(money, count(nt.presenca)) * 100 >= 75, 'Aprovado',
+'Reprovado') as 'Status'
+
+from Aluno A
+inner join Notas NT on A.Id = nt.aluno
+group by a.Nome
