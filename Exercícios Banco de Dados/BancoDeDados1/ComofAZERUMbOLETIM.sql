a9488e07093d040faf93049bb8f1538598c27408
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-22 13:37:31 -0300

    exercicios

---
+++ Exercícios Banco de Dados/BancoDeDados1/ComofAZERUMbOLETIM.sql
@@ -0,0 +1,7 @@
+﻿select a.Aluno, sum (n.Nota) / count (d.Data) as 'Nota' from Alunos a 
+inner join TurmaAlunos tr on a.Id = tr.Aluno 
+inner join Diario d on tr.Turma = d.Turma
+left join Notas n on d.Id = n.DiarioId and a.Id = n.Aluno
+where tr.Turma  = 3
+group by a.Aluno
+
---
+++ Exercícios Banco de Dados/BancoDeDados1/ExercicioSegundaDiario.sql
@@ -0,0 +1,13 @@
+﻿
+select 
+
+d.[Data],
+tr.Aluno,
+isnull (n.Nota,0) as 'nota',
+iif (isnull (p.Id,0) > 0, 'Presente', 'Ausente') as 'Presenças'
+from Diario d 
+inner join Turmas t on d.Turma = t.Id
+inner join TurmaAlunos tr on t.Id = tr.Turma
+inner join Alunos a on tr.Aluno = a.Id
+left join Presenca p on d.Id = p.Diario and d.Id = p.Diario
+left join Notas n on tr.Aluno = n.Aluno and d.Id = n.DiarioId
