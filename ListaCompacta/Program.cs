8535843aa115edc3a308f606d4cd03980241e806
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 11:17:34 -0300

    country_roads_take_me_home.mp3

---
+++ ListaCompacta/Program.cs
@@ -0,0 +1,17 @@
+﻿using ListaCompacta.Criacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaCompacta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new CriandoLista();
+        }
+    }
+}
