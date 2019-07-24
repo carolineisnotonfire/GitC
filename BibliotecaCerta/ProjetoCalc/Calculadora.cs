33d3a69bdf32e108cfb17ca241d3a685b5144b8e
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 17:26:13 -0300

    changes

---
+++ BibliotecaCerta/ProjetoCalc/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCalc
+{
+    public class Calculadora
+    {
+        public static int CalculaAdicao(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        public static int CalculaSubtracao(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+        public static int CalculaMultiplicacao(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+        public static int CalculaDivisao(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
