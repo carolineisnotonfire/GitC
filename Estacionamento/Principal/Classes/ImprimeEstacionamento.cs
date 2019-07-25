34e56321277f68a5a31438217eaab49c44d254ab
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-25 17:22:19 -0300

    não era o que eu queria, mas talvez o que eu precisava

---
+++ Estacionamento/Principal/Classes/ImprimeEstacionamento.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal.Classes
+{
+    class ImprimeEstacionamento
+    {
+        string[,] listaCarros = new string[3, 5];
+        int IDCarro = 0;
+        public void inserir()
+        {
+            for (int i = 0; i < listaCarros.GetLength(0); i++)
+            {
+                Console.WriteLine("Placa: ");
+                var placa = Console.ReadLine();
+
+                Console.WriteLine("Modelo: ");
+                var modelo = Console.ReadLine();
+
+                var entrada = Convert.ToString(DateTime.Now);
+
+                listaCarros[i, 0] = (IDCarro++).ToString();
+                listaCarros[i, 1] = placa;
+                listaCarros[i, 2] = modelo;
+                listaCarros[i, 3] = entrada;
+                listaCarros[i, 4] = null;                
+
+            }
+        }
+        public void digitaSaida ()
+        {
+            for (int i = 0; i < listaCarros.GetLength(0); i++)
+            {
+                if (listaCarros[i,3] == null)
+                {
+                    listaCarros[i, 3] = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+                }
+            }
+        }
+    }
+
+}
---
+++ Estacionamento/Principal/Program.cs
@@ -0,0 +1,21 @@
+﻿using Principal.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimeEstacionamento imp = new ImprimeEstacionamento();
+            imp.inserir();
+
+            Console.ReadKey();
+        }
+    }
+
+}
