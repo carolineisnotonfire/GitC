e18d1bbdc9ac9eb0aa06071d3e2d07ba010bd587
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 15:45:50 -0300

    usando as listas e é isso.

---
+++ BarDoBardo/Classes/Acoes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarDoBardo.Classes
+{
+    class Acoes
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; set; }
+    }
+}
---
+++ BarDoBardo/Program.cs
@@ -0,0 +1,39 @@
+﻿using BarDoBardo.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarDoBardo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Acoes> minhaLista = new List<Acoes>();
+            for (int i = 0; i < 3; i++)
+            {
+                minhaLista.Add(new Acoes()
+                {
+                    Nome = RetornaValores("Nome"),
+                    Quantidade = int.Parse(RetornaValores("Quantidade")),
+                    Valor = double.Parse(RetornaValores("Valor"))
+                });
+            }
+
+            foreach (Acoes item in minhaLista)
+            {
+                Console.WriteLine($"Lanches disponíveis: {item.Nome} Preço: {item.Valor}" +
+                    $" Quantidade disponível: {item.Quantidade}");
+                Console.ReadKey();
+            }
+        }
+        public static string RetornaValores(string nome)
+        {
+            Console.WriteLine($"Informe o valor para o campo: {nome}");
+            return Console.ReadLine();
+        }
+    }
+}
