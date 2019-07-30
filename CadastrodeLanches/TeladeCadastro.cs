cf3eef10417ab2c6f46055561d4f65014ad53b46
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-30 10:48:28 -0300

    cadastrinho

---
+++ CadastrodeLanches/TeladeCadastro.cs
@@ -0,0 +1,36 @@
+﻿using CadastrodeLanches.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastrodeLanches
+{
+    public partial class TeladeCadastro : Form
+    {
+        public TeladeCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void TeladeCadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+        public Lanche novoLanche = new Lanche();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            novoLanche.Nome = tbxNome.Text;
+            novoLanche.Quantidade = (int)nrQuant.Value;
+            novoLanche.Valor = double.Parse(tbxValor.Text);
+
+            this.Close();
+        }
+    }
+}
---
+++ MyFirstWinForms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int countPress = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Click event: {countPress}");
+            countPress++;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("acho que não hihi");
+            this.Close();
+        }
+    }
+}
